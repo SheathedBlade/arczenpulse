55e1fbfe2eea8555055b0fec905324576c3eb369
Andrew Chuah <warlordpluto@gmail.com>
2022-05-07 19:08:43 -0400

    added fonts

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { format } from 'date-fns'
 
 const Home: NextPage = () => {
   return (
