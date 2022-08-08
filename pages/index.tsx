5de07a16ed38df06080354358c83c17579ca76f6
Andrew Chuah <warlordpluto@gmail.com>
2022-08-08 06:05:55 -0400

    disbled eslint for now, will decide spa or not

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
+//import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import { format } from 'date-fns'
+//import { format } from 'date-fns'
 
 const Home: NextPage = () => {
   return (
