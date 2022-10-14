addc74a84ab23c9e35324e7e0bdbd08b95283469
Andrew Chuah <warlordpluto@gmail.com>
2022-10-14 15:55:48 -0400

    started on home page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,10 +1,10 @@
 const NotFound = () => {
     return (
         <div className="container flex justify-center content-center flex-col bg-cream dark:bg-wood max-w-md rounded-lg">
-            <h1 className="text-wood dark:text-cream text-center text-5xl uppercase mb-3 p-5 font-adventProHeader">
+            <h1 className="text-wood dark:text-cream text-center text-5xl uppercase mb-3 p-5 font-adventProHeader font-bold">
                 Page Not Found
             </h1>
-            <hr className="bg-wood dark:bg-cream my-auto mx-10 p-1 rounded-lg border-wood dark:border-l-cream" />
+            <hr className="bg-wood dark:bg-cream my-auto mx-10 p-1 rounded-lg border-wood dark:border-cream" />
             <h2 className="font-titillium text-wood dark:text-cream text-center p-10 text-3xl">
                 Click here to head back to the main page!
             </h2>
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,17 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
     return (
-        <div className="flex">
-            <p>HELLO</p>
+        <div className="flex container justify-evenly">
+            <div className="flex container flex-col bg-cream dark:bg-wood rounded-lg justify-center content-center max-w-lg">
+                <h1 className="font-adventProHeader font-black text-5xl text-wood dark:text-cream mb-3 p-4 text-center uppercase">
+                    Andrew Chuah
+                </h1>
+                <hr className="bg-wood dark:bg-cream my-auto mx-10 p-1 rounded-lg border-wood dark:border-cream" />
+                <p className="font-titillium p-5 text-center text-wood dark:text-cream text-lg">
+                    I am an aspiring full-stack developer and software engineer.
+                </p>
+            </div>
+            <div className="flex container flex-col bg-cream dark:bg-wood rounded-lg justify-center content-center max-w-lg"></div>
         </div>
     )
 }
