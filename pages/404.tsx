1d675173be30977bf780e7176fb5f64c64d7c880
Andrew Chuah <warlordpluto@gmail.com>
2022-10-14 11:39:11 -0400

    updated text

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,11 +1,11 @@
 const NotFound = () => {
     return (
-        <div className="container flex justify-center content-center flex-col bg-cream max-w-md rounded-lg">
-            <h1 className="text-wood text-center text-5xl uppercase mb-3 p-5 font-adventProHeader">
+        <div className="container flex justify-center content-center flex-col bg-cream dark:bg-wood max-w-md rounded-lg">
+            <h1 className="text-wood dark:text-cream text-center text-5xl uppercase mb-3 p-5 font-adventProHeader">
                 Page Not Found
             </h1>
-            <hr className="bg-wood my-auto mx-10 p-1 rounded-lg border-wood" />
-            <h2 className="font-titillium text-wood text-center p-10 text-3xl">
+            <hr className="bg-wood dark:bg-cream my-auto mx-10 p-1 rounded-lg border-wood dark:border-l-cream" />
+            <h2 className="font-titillium text-wood dark:text-cream text-center p-10 text-3xl">
                 Click here to head back to the main page!
             </h2>
         </div>
