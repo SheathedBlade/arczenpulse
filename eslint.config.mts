07bd175d0c8f92ea7323564426fd55b1f411ae59
Andrew Chuah <warlordpluto@gmail.com>
2026-03-12 01:47:58 -0400

    updated eslint

---
+++ eslint.config.mts
@@ -0,0 +1,34 @@
+import js from '@eslint/js'
+import pluginReact from 'eslint-plugin-react'
+import { defineConfig } from 'eslint/config'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } }
+  },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      'react-refresh/only-export-components': [
+        'warn',
+        {
+          allowConstantExport: true
+        }
+      ],
+
+      'no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_'
+        }
+      ]
+    }
+  }
+])
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,21 +1,22 @@
-import { useState } from "react";
+import { useState } from 'react'
 
 const navItems = [
-  { name: "Home", link: "/" },
-  { name: "About", link: "/about" },
-  { name: "Works", link: "/works" },
-  { name: "Contact", link: "/contact" },
-];
+  { name: 'Home', link: '/' },
+  { name: 'About', link: '/about' },
+  { name: 'Works', link: '/works' },
+  { name: 'Contact', link: '/contact' }
+]
 
 const Navbar = () => {
-  const [hovered, setHovered] = useState(null);
-
+  const [hovered, setHovered] = useState(null)
 
   return (
-    <div className="flex items-center justify-center bg-sakura-day-bg h-20 dark:bg-sakura-night-bg">
-      <h1 className="font-dmmono text-3xl font-bold -tracking-wider text-sakura-day-text dark:text-sakura-night-text">Terystal</h1>
+    <div className="bg-sakura-day-bg dark:bg-sakura-night-bg flex h-20 items-center justify-center">
+      <h1 className="font-dmmono text-sakura-day-text dark:text-sakura-night-text text-3xl font-bold -tracking-wider">
+        Terystal
+      </h1>
     </div>
   )
 }
 
-export default Navbar;
+export default Navbar
