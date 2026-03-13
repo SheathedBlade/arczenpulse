479ecf7c1aea96e0f644cbcfd9b2d6223875ae05
Andrew Chuah <warlordpluto@gmail.com>
2026-03-12 21:44:04 -0400

    added tanstack router

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
+import { tanstackRouter } from '@tanstack/router-plugin/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr({ svgrOptions: { icon: true } })],
+  plugins: [
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true
+    }),
+    react(),
+    svgr({ svgrOptions: { icon: true } })
+  ],
   server: {
     watch: {
       usePolling: true
