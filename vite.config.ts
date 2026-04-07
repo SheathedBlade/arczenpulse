43d2dcecaf837c4ce04126e945e5a63572f69371
Andrew Chuah <warlordpluto@gmail.com>
2026-04-06 20:42:24 -0400

    commented out pwa

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path';
 import { defineConfig } from 'vite';
 import Sitemap from 'vite-plugin-sitemap';
 import svgr from 'vite-plugin-svgr';
-import { VitePWA } from 'vite-plugin-pwa';
+// import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -28,41 +28,41 @@ export default defineConfig({
       }
     }),
     svgr({ svgrOptions: { icon: true } }),
-    Sitemap({ hostname: 'https://terystal.dev' }),
-    VitePWA({
-      registerType: 'autoUpdate',
-      injectRegister: 'auto',
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,webp,svg,woff,woff2}'],
-        runtimeCaching: [
-          {
-            urlPattern: ({ url }) => url.origin === 'https://terystal.dev',
-            handler: 'NetworkFirst',
-            options: {
-              cacheName: 'html-cache',
-              expiration: {
-                maxEntries: 32,
-                maxAgeSeconds: 86400
-              },
-              networkTimeoutSeconds: 3
-            }
-          },
-          {
-            urlPattern: ({ url }) =>
-              url.origin === 'https://terystal.dev' &&
-              /\.(ico|png|webp|svg|jpg|jpeg|gif)$/.test(url.pathname),
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'image-cache',
-              expiration: {
-                maxEntries: 64,
-                maxAgeSeconds: 604800
-              }
-            }
-          }
-        ]
-      }
-    })
+    Sitemap({ hostname: 'https://terystal.dev' })
+    // VitePWA({
+    //   registerType: 'autoUpdate',
+    //   injectRegister: 'auto',
+    //   workbox: {
+    //     globPatterns: ['**/*.{js,css,html,ico,png,webp,svg,woff,woff2}'],
+    //     runtimeCaching: [
+    //       {
+    //         urlPattern: ({ url }) => url.origin === 'https://terystal.dev',
+    //         handler: 'NetworkFirst',
+    //         options: {
+    //           cacheName: 'html-cache',
+    //           expiration: {
+    //             maxEntries: 32,
+    //             maxAgeSeconds: 86400
+    //           },
+    //           networkTimeoutSeconds: 3
+    //         }
+    //       },
+    //       {
+    //         urlPattern: ({ url }) =>
+    //           url.origin === 'https://terystal.dev' &&
+    //           /\.(ico|png|webp|svg|jpg|jpeg|gif)$/.test(url.pathname),
+    //         handler: 'CacheFirst',
+    //         options: {
+    //           cacheName: 'image-cache',
+    //           expiration: {
+    //             maxEntries: 64,
+    //             maxAgeSeconds: 604800
+    //           }
+    //         }
+    //       }
+    //     ]
+    //   }
+    // })
   ],
   server: {
     watch: {
